227478ab5267815c871dd64019e8228d2ee4fe28
Eddy Gao <gaoeddy@gmail.com>
2013-02-03 16:09:33 -0500

    fix moving stacks around

--- src/com/eddygao/MainView.java
+++ src/com/eddygao/MainView.java
@@ -18,7 +18,7 @@ import com.eddygao.Card.Suit;
 import com.eddygao.Deck.deckType;
 import com.eddygao.mysolitaire.R;
 
-public class MainView extends View implements OnTouchListener  {
+public class MainView extends View implements OnTouchListener {
 	private Paint mCanvasPaint;
 
 	private Bitmap mCacheBitmap;
@@ -35,7 +35,7 @@ public class MainView extends View implements OnTouchListener  {
 
 	private Card activeCard;
 	ArrayList<Drawable> spadeImages;
-	
+
 	public MainView(Context context) {
 
 		super(context);
@@ -45,53 +45,56 @@ public class MainView extends View implements OnTouchListener  {
 		mCanvasPaint.setColor(0xFF228B22); // Green background
 		mCanvasPaint.setAntiAlias(false);
 
-
 		Drawable back = getResources().getDrawable(R.drawable.cardback);
 
 		spadeImages = new ArrayList<Drawable>();
-		
+
 		for (int i = 1; i <= 13; i++) {
-			spadeImages.add(getResources().getDrawable(getResources().getIdentifier("spade"+i, "drawable", "com.eddygao.mysolitaire")));
+			spadeImages.add(getResources().getDrawable(
+					getResources().getIdentifier("spade" + i, "drawable",
+							"com.eddygao.mysolitaire")));
 		}
 
 		decks = new ArrayList<Deck>();
-		decks.add(new Deck(10, 10, mCardSize.width(), mCardSize.height(), deckType.tableau));
+		decks.add(new Deck(10, 10, mCardSize.width(), mCardSize.height(),
+				deckType.tableau));
 
 		for (int i = 13; i > 0; i--) {
-			decks.get(0).addCard(new Card(i, Suit.spade, spadeImages.get(i-1), decks.get(0), true));
+			decks.get(0).addCard(
+					new Card(i, Suit.spade, spadeImages.get(i - 1), decks
+							.get(0), true));
 		}
 
-		decks.add(new Deck(300, 10, mCardSize.width(), mCardSize.height(), deckType.foundation));
-		decks.get(1).addCard(new Card(10, Suit.spade, back, decks.get(1), true));
-		decks.add(new Deck(600, 10, mCardSize.width(), mCardSize.height(), deckType.waste));
+		decks.add(new Deck(300, 10, mCardSize.width(), mCardSize.height(),
+				deckType.foundation));
+		decks.get(1)
+				.addCard(new Card(10, Suit.spade, back, decks.get(1), true));
+		decks.add(new Deck(600, 10, mCardSize.width(), mCardSize.height(),
+				deckType.waste));
+		decks.add(new Deck(900, 10, mCardSize.width(), mCardSize.height(),
+				deckType.tableau));
 	}
 
-	
-	
-	
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
-		
-		
+
 		// Store current screen size
 		mScreenSize.set(0, 0, w, h);
-//		Log.v("size ", mScreenSize.toString());
+		// Log.v("size ", mScreenSize.toString());
 		// Calculate card and decks sizes and positions
 		int cw = w / 11;
 		mCardSize.set(0, 0, cw, (int) (cw * 1.5));
-
 
 		int freeSize = w - cw * 7;
 		mCardCap = freeSize / (6 + 4 * 2);
 
 		int cy = (int) (mScreenSize.height() * 0.35);
-		
-		
+
 		for (Deck curDeck : decks) {
 			curDeck.resize(mCardSize.width(), mCardSize.height());
 		}
-		
+
 	}
 
 	@Override
@@ -109,32 +112,31 @@ public class MainView extends View implements OnTouchListener  {
 			for (Deck curDeck : decks) {
 				curDeck.draw(canvas);
 			}
-		} 
+		}
 	}
-
 
 	public Card cardUnderTouch(float x, float y) {
 		for (Deck deck : decks) {
-			for (int i = deck.getSize()-1; i >= 0; i--) {
-				Card card =  deck.getCard(i);
-				if (card.inCard(x,y)) {
+			for (int i = deck.getSize() - 1; i >= 0; i--) {
+				Card card = deck.getCard(i);
+				if (card.inCard(x, y)) {
 					return card;
 				}
 			}
 		}
 		return null;
 	}
-	
+
 	public Deck deckUnderTouch(float x, float y) {
-		for (Deck deck: decks) {
-			if (x > deck.getX() && x < deck.getWidth() + deck.getX() && 
-				y > deck.getY() && y < deck.getHeight() + deck.getY()) {
+		for (Deck deck : decks) {
+			if (x > deck.getX() && x < deck.getWidth() + deck.getX()
+					&& y > deck.getY() && y < deck.getHeight() + deck.getY()) {
 				return deck;
 			}
 		}
 		return null;
 	}
-	
+
 	@Override
 	public boolean onTouch(View v, MotionEvent e) {
 		int action = e.getAction();
@@ -142,13 +144,13 @@ public class MainView extends View implements OnTouchListener  {
 			oldX = e.getX();
 			oldY = e.getY();
 			Card card = cardUnderTouch(e.getX(), e.getY());
-			if(card != null) {
+			if (card != null) {
 				activeCard = card;
 			}
 		}
 		if (action == MotionEvent.ACTION_MOVE) {
 			if (activeCard != null) {
-					moveCard(activeCard, e.getX(), e.getY());
+				moveCard(activeCard, e.getX(), e.getY());
 
 			}
 		}
@@ -157,7 +159,7 @@ public class MainView extends View implements OnTouchListener  {
 				cardReleased(activeCard, e.getX(), e.getY());
 			activeCard = null;
 		}
-		
+
 		return true;
 	}
 
@@ -165,59 +167,68 @@ public class MainView extends View implements OnTouchListener  {
 		Deck parent = card.getParent();
 		if (parent.getType() == deckType.tableau) {
 			for (int i = 0; i < parent.getSize(); i++) {
-				if (parent.getCard(i).getValue() <= card.getValue()) parent.getCard(i).moveByDelta((int)(newX - oldX), (int)(newY - oldY));
+				if (parent.getCard(i).getValue() <= card.getValue())
+					parent.getCard(i).moveByDelta((int) (newX - oldX),
+							(int) (newY - oldY));
 			}
-		}
-		else card.moveByDelta((int)(newX - oldX), (int)(newY - oldY));
+		} else
+			card.moveByDelta((int) (newX - oldX), (int) (newY - oldY));
 		oldX = newX;
 		oldY = newY;
 		invalidate();
 	}
-	
+
 	public boolean legalMove(Card card, Deck destination) {
-		//TODO implement
+		// TODO implement
 		return true;
 	}
-	
-	public void moveStackToDeck(Deck parent, Deck dest, int value) {
+
+	public void moveStackToDeck(Deck parent, Deck dest, Card card) {
+		ArrayList<Card> toMove = new ArrayList<Card>();
+
 		for (int i = 0; i < parent.getSize(); i++) {
 			Card c = parent.getCard(i);
-			if (c.getValue() <= value) {
-				parent.removeCard(c);
-				dest.addCard(c);
+			if (c.getValue() < card.getValue()) {
+				toMove.add(c);
 			}
 		}
+		for (Card c : toMove) {
+			parent.removeCard(c);
+		}
+		dest.addCard(card);
+		for (Card c : toMove) {
+			dest.addCard(c);
+		}
 	}
-	
+
 	public void cardReleased(Card card, float x, float y) {
 		Deck parent = card.getParent();
 		parent.removeCard(card);
 		Card position = cardUnderTouch(x, y);
 
 		if (position != null && legalMove(card, position.getParent())) {
-			position.getParent().addCard(card);
-			if (parent.getType() == deckType.tableau) 
-				moveStackToDeck(parent, position.getParent(), card.getValue());
-			}
-		else {
+			
+			if (parent.getType() == deckType.tableau)
+				moveStackToDeck(parent, position.getParent(), card);
+			else 
+				position.getParent().addCard(card);
+		} else {
 			Deck deckPosition = deckUnderTouch(x, y);
 			if (null != deckPosition) {
-				deckPosition.addCard(card);
-				if (parent.getType() == deckType.tableau) 
-					moveStackToDeck(parent, deckPosition, card.getValue());
+				
+				if (parent.getType() == deckType.tableau)
+					moveStackToDeck(parent, deckPosition, card);
+				else 
+					deckPosition.addCard(card);
+			} else {
+				
+				if (parent.getType() == deckType.tableau)
+					moveStackToDeck(parent, parent, card);
+				else parent.addCard(card);
 			}
-			else{
-				parent.addCard(card);
-				if (parent.getType() == deckType.tableau) 
-					moveStackToDeck(parent, parent, card.getValue());
-			}
-			
+
 		}
 		invalidate();
 	}
 
-
-
-
-	
 }
